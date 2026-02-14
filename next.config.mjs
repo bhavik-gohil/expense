4ab63a63f5ab2a5d4487e1a83be52dcddf870dba
bhavik <bhavik@example.com>
2026-02-14 11:40:53 +0530

    ok

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  output: "export",
 };
 
 export default nextConfig;
