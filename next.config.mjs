e4620ef0a40745810707b1498c6bf58c3fa2c524
bhavik <bhavik@example.com>
2026-02-14 11:56:11 +0530

    ok

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
   output: "export",
+  basePath: "/expense",
+  assetPrefix: "/expense/",
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
